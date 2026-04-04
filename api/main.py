b4ce7ce4b451f40c9e4127a2f6844ddfc0ce6a9b
Shanawazw <shanawazsa@yahoo.com>
2026-04-04 11:02:38 +0530

    main.py

--- api/main.py
+++ api/main.py
@@ -21,7 +21,9 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://app.eligicheck.in",
+    "https://app.eligicheck.in"],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
